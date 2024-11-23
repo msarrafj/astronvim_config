e3b959fa6d31b44642a641f6cff5f5005dac97b1
Sarraf <m.sarraf.j@gmail.com>
2024-11-23 01:36:17 -0800

    minor

--- lua/plugins/gitsign.lua
+++ lua/plugins/gitsign.lua
@@ -1,10 +1,14 @@
-require("gitsigns").setup {
-  preview_config = {
-    -- Options passed to nvim_open_win
-    border = "single",
-    style = "minimal",
-    relative = "cursor",
-    row = 0,
-    col = 1,
+return {
+  {
+    require("gitsigns").setup {
+      preview_config = {
+        -- Options passed to nvim_open_win
+        border = "single",
+        style = "minimal",
+        relative = "cursor",
+        row = 0,
+        col = 1,
+      },
+    },
   },
 }
